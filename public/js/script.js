928f6b334fd9ee990b733923b889936a343a27aa
Issameddine <issameddine.matoug29@gmail.com>
2025-06-15 16:31:44 +0200

    SAV14

--- public/js/script.js
+++ public/js/script.js
@@ -110,3 +110,21 @@ window.addEventListener("load", () => {
     }, 1000);
   }
 });
+new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoHeight: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
+});
